70915f8024e1448f93192bdf8dd95e95ae4d640d
Mark Williams <mark@Marks-MacBook-Pro.local>
2019-05-14 17:35:09 +0100

    Upgrade to spfx 1.8

--- src/webparts/itemStacker/ItemStackerWebPart.ts
+++ src/webparts/itemStacker/ItemStackerWebPart.ts
@@ -1,13 +1,8 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import { Version, DisplayMode } from '@microsoft/sp-core-library';
-import {
-  BaseClientSideWebPart,
-  IPropertyPaneConfiguration,
-  IWebPartPropertiesMetadata,
-  IPropertyPaneDropdownOption,
-  PropertyPaneDropdown
-} from '@microsoft/sp-webpart-base';
+import { BaseClientSideWebPart, IWebPartPropertiesMetadata } from "@microsoft/sp-webpart-base";
+import { IPropertyPaneConfiguration, IPropertyPaneDropdownOption, PropertyPaneDropdown } from "@microsoft/sp-property-pane";
 import SiteServices from "./services/SiteServices";
 import * as strings from 'ItemStackerWebPartStrings';
 import ItemStacker from './components/ItemStacker';
